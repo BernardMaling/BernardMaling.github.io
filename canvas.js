0ba9e1835912cb966d667617d1db4c926de5b268
BernardMaling <bernardmalingio@gmail.com>
2017-07-25 16:35:51 -0700

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -142,8 +142,8 @@ function init(){
     var radius = Math.random() * 3 + 1 ;
     var x = Math.random() * (innerWidth - radius * 2) + radius;
     var y = Math.random() * (innerHeight - radius * 2) + radius;
-    var dx = (Math.random() - 2);
-    var dy = (Math.random() - 2);
+    var dx = (Math.random() - 0.5);
+    var dy = (Math.random() - 0.5);
     circleArray.push(new Circle(x, y, dx, dy, radius));
 
   }
